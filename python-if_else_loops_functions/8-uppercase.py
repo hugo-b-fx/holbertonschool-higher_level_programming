ccf358ae7e7dbd0e03f2c04872a0919bd261b5e0
hugo-b-fx <gucci60610@gmail.com>
2025-01-16 21:01:51 +0100

    all tasks

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for character in str:
+        if ord('a') <= ord(character) <= ord('z'):
+            uppercase_character = chr(ord(character) - 32)
+        else:
+            uppercase_character = character
+        print("{}".format(uppercase_character), end="")
+    print()
