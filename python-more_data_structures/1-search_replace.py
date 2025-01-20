0283608060b7668c0601622c0243e56703f74ce2
hugo-b-fx <gucci60610@gmail.com>
2025-01-20 15:12:15 +0100

    dd

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3new_list
+def search_replace(my_list, search, replace):
+
+    new_list = my_list.copy()
+
+    for number in range(len(new_list)):
+        if new_list[number] == search:
+            new_list[number] = replace
+
+    return new_list
