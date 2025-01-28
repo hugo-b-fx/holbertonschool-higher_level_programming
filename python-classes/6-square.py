ed5ae0daa0f72ea82c7c1d9ad2aa057870c87a73
hugo-b-fx <gucci60610@gmail.com>
2025-01-28 14:48:56 +0100

    dd

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python3
+'''
+Creation of a class: Square
+Manipulations(getter, setter, my_print)
+'''
+
+
 class Square:
+    '''Classe named Square'''
+
     def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-
-        if not isinstance(position, tuple) or len(position) != 2 or \
-           not all(isinstance(i, int) and i >= 0 for i in position):
-            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = position
+        if (not isinstance(position, tuple) or len(position) != 2 or
+                not all(isinstance(i, int) and i >= 0 for i in position)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    def area(self):
+        area = self.__size * self.__size
+        return area
 
     @property
     def size(self):
@@ -20,10 +31,10 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
+        return self.__size
 
     @property
     def position(self):
@@ -31,20 +42,16 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if not isinstance(value, tuple) or len(value) != 2 or \
-           not all(isinstance(i, int) and i >= 0 for i in value):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(i, int) and i >= 0 for i in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-        else:
-            self.__position = value
-
-    def area(self):
-        return self.__size * self.__size
+        self.__position = value
 
     def my_print(self):
-        if not self.__size:
+
+        if self.__size == 0:
             print()
         else:
-            for _ in range(self.__position[1]):
-                print()
-            for _ in range(self.__size):
-                print(" " * self.__position[0] + "#" * self.__size)
+            x, y = self.__position
+            print(("\n" * y + (" " * x + "#" * self.__size + "\n")
+                  * self.__size), end="")
