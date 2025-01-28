9437429a01e4709ca9cc8fb0c2f455d6c72868ca
hugo-b-fx <gucci60610@gmail.com>
2025-01-28 14:19:55 +0100

    dd

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
 """
 script defining a square
 """
 
+
 class Square:
-    pass
+    ""
+    ""
--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-"""
-script defining a square
-"""
-
 class Square:
-    """
-    ddd
-    """
     def __init__(self, size=0):
         if type(size) is not int:
             raise TypeError("size must be an integer")
