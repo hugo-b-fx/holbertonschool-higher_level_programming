8d22d4590a463ab0e73fc070501fbbc8e687c77a
hugo-b-fx <gucci60610@gmail.com>
2025-01-28 14:42:48 +0100

    dd

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,5 +5,4 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise TypeError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
